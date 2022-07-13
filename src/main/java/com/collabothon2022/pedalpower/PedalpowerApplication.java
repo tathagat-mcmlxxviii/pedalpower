bbadd77f9c40b7d38c2c10948c459a8b3de958a3
Tathagat <tathagat@Tathagats-MacBook-Air.local>
2022-07-13 14:54:10 +0200

    basic spring boot

---
+++ src/main/java/com/collabothon2022/pedalpower/PedalpowerApplication.java
@@ -0,0 +1,13 @@
+package com.collabothon2022.pedalpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedalpowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedalpowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collabothon2022/pedalpower/PedalpowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabothon2022.pedalpower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedalpowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
