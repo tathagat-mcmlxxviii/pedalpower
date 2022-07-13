829e0868cf00b9213edca16a7057b02850a84d26
Tathagat <tathagat@Tathagats-MacBook-Air.local>
2022-07-13 20:00:01 +0200

    compile issues

--- src/test/java/com/collabothon2022/pedalpower/external/CityInfoApiStubTest.java
+++ src/test/java/com/collabothon2022/pedalpower/external/CityInfoApiStubTest.java
@@ -1,14 +1,16 @@
 package com.collabothon2022.pedalpower.external;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import com.collabothon2022.pedalpower.external.api.CityInfoApi;
 import com.collabothon2022.pedalpower.external.api.CityInfoApiStub;
 import com.collabothon2022.pedalpower.external.api.ExchangeEntry;
 import com.collabothon2022.pedalpower.external.api.ExchangeRequest;
 import com.collabothon2022.pedalpower.external.api.ExchangeResponse;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
+import com.collabothon2022.pedalpower.persistence.model.City;
 
 public class CityInfoApiStubTest {
 
@@ -17,7 +19,8 @@ public class CityInfoApiStubTest {
 	@Test
 	public void shouldGetExchangeOptions() {
 		// when
-		List<ExchangeEntry> exchangeOptionList = testee.getExchangeOptionList();
+		City city = null; // TODO
+		List<ExchangeEntry> exchangeOptionList = testee.getExchangeOptionList(city);
 		// then
 		Assertions.assertTrue(exchangeOptionList.size() > 0);
 	}
@@ -25,10 +28,11 @@ public class CityInfoApiStubTest {
 	@Test
 	public void shouldExecuteExchange() {
 		// given
-		List<ExchangeEntry> exchangeOptionList = testee.getExchangeOptionList();
+		City city = null; // TODO
+		List<ExchangeEntry> exchangeOptionList = testee.getExchangeOptionList(city);
 		ExchangeEntry exchangeEntry = exchangeOptionList.get(0);
 		// when
-		ExchangeResponse exchangeResponse = testee.executeExchange(new ExchangeRequest(1, exchangeEntry.getBuyUrl()));
+		ExchangeResponse exchangeResponse = testee.executeExchange(new ExchangeRequest(1L, exchangeEntry.getBuyUrl()));
 		// then
 		Assertions.assertNotNull(exchangeResponse);
 		Assertions.assertNotNull(exchangeResponse.getBase64TicketImg());
